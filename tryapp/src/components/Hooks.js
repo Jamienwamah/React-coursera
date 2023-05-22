ffdd3c237335209a6f5fd0054381630f80cf04c9
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-22 23:56:33 +0100

    react

--- tryapp/src/components/Hooks.js
+++ tryapp/src/components/Hooks.js
@@ -1,19 +1,76 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function InputComponent() { 
-  const [inputText, setText] = useState('hello'); 
+// export default function InputComponent() { 
+//   const [inputText, setText] = useState('hello'); 
 
-  function handleChange(e) { 
-    setText(e.target.value); 
-  } 
+//   function handleChange(e) { 
+//     setText(e.target.value); 
+//   } 
+
+//   return ( 
+//     <> 
+//       <input value={inputText} onChange={handleChange} /> 
+//       <p>You typed: {inputText}</p> 
+//       <button onClick={() => setText('hello')}> 
+//         Reset 
+//       </button> 
+//     </> 
+//   ); 
+// } 
+
+
+import { useState } from 'react'; 
+
+export default function RegisterForm() { 
+  const [form, setForm] = useState({ 
+    firstName: 'Luke', 
+    lastName: 'Jones', 
+    email: 'lukeJones@sculpture.com', 
+  }); 
 
   return ( 
     <> 
-      <input value={inputText} onChange={handleChange} /> 
-      <p>You typed: {inputText}</p> 
-      <button onClick={() => setText('hello')}> 
-        Reset 
-      </button> 
+      <label> 
+        First name: 
+        <input 
+          value={form.firstName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              firstName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Last name: 
+        <input 
+          value={form.lastName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              lastName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Email: 
+        <input 
+          value={form.email} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              email: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <p> 
+        {form.firstName}{' '} 
+        {form.lastName}{' '} 
+        ({form.email})
+      </p> 
     </> 
   ); 
 } 
