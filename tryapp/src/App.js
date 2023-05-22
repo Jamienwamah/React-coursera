a6d06399d708799090a490d0565800c91cbc8fb8
oishbdh <ikechukwuarinze614@gmail.com>
2023-05-22 21:15:47 +0100

    hooks

--- tryapp/src/App.js
+++ tryapp/src/App.js
@@ -1,15 +1,27 @@
-import Btn from './components/Btn';
-import Header from './components/Header';
-import Nav from './components/Nav';
+// import Btn from './components/Btn';
+// import Header from './components/Header';
+// import Nav from './components/Nav';
 
 function App() {
+
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    console.log(randomNum)
+    let userInput = prompt('type a number');
+    alert('Computer number: ${randomNum}, Your guess: ${userInput}')
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-      <Btn />
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
     </div>
   );
 }
 
 export default App;
+
+      {/* <Header />
+      <Nav />
+  <Btn /> */}
+    
